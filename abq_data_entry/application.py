b5bc26ba3177efdb5ed4c9dd4a9d77a96f8e9d77
neo <zrhett@163.com>
2019-06-18 21:27:36 +0800

    up to page 291

--- abq_data_entry/application.py
+++ abq_data_entry/application.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import ttk
+from tkinter import filedialog
 from tkinter import messagebox
 from datetime import datetime
 from . import views as v
@@ -11,84 +12,129 @@ class Application(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.title('ABQ Data Entry Application')
+
+        self.title("ABQ Data Entry Application")
         self.resizable(width=False, height=False)
 
-        ttk.Label(self, text='ABQ Data Entry Application', font=('TkDefaultFont', 16)).grid(row=0, pady=5)
-        datestring = datetime.today().strftime('%Y-%m-%d')
-        default_filename = f'abq_data_record_{datestring}.csv'
+        # new code for ch7
+        datestring = datetime.today().strftime("%Y-%m-%d")
+        default_filename = "abq_data_record_{}.csv".format(datestring)
         self.filename = tk.StringVar(value=default_filename)
         self.data_model = m.CSVModel(filename=self.filename.get())
-
-        self.settings_model = m.SettingModel()
+        self.settings_model = m.SettingsModel()
         self.load_settings()
 
         self.callbacks = {
             'file->select': self.on_file_select,
-            'file->quit': self.quit
+            'file->quit': self.quit,
+            'show_recordlist': self.show_recordlist,
+            'new_record': self.open_record,
+            'on_open_record': self.open_record,
+            'on_save': self.on_save
         }
+
         menu = v.MainMenu(self, self.settings, self.callbacks)
         self.config(menu=menu)
 
-        self.recordform = v.DataRecordForm(self, m.CSVModel.fields, self.settings)
+        # The data record form
+        self.recordform = v.DataRecordForm(
+            self, m.CSVModel.fields, self.settings, self.callbacks)
         self.recordform.grid(row=1, padx=10, sticky='NSEW')
 
+        # The data record list
         self.recordlist = v.RecordList(self, self.callbacks)
         self.recordlist.grid(row=1, padx=10, sticky='NSEW')
-
-        self.savebutton = ttk.Button(self, text='Save', command=self.on_save)
-        self.savebutton.grid(sticky=tk.E, row=2, padx=10)
+        self.populate_recordlist()
 
         # status bar
         self.status = tk.StringVar()
         self.statusbar = ttk.Label(self, textvariable=self.status)
-        self.statusbar.grid(sticky=(tk.W + tk.E), row=3, padx=10)
+        self.statusbar.grid(sticky="we", row=2, padx=10)
 
         self.records_saved = 0
 
-    def load_settings(self):
-        """Load settings into our self.settings dict."""
-        vartypes = {
-            'bool': tk.BooleanVar,
-            'str': tk.StringVar,
-            'int': tk.IntVar,
-            'float': tk.DoubleVar
-        }
+    def show_recordlist(self):
+        """Show the recordform"""
 
-        self.settings = {}
-        for key, data in self.settings_model.variables.items():
-            vartype = vartypes.get(data['type'], tk.StringVar)
-            self.settings[key] = vartype(value=data['value'])
+        self.recordlist.tkraise()
 
-        for var in self.settings.values():
-            var.trace('w', self.save_settings)
+    def populate_recordlist(self):
+        try:
+            rows = self.data_model.get_all_records()
+        except Exception as e:
+            messagebox.showerror(
+                title='Error',
+                message='Problem reading file',
+                detail=str(e)
+            )
+        else:
+            self.recordlist.populate(rows)
 
-    def save_settings(self, *args):
-        """Save the current settings to a preferences file"""
-        for key, variable in self.settings.items():
-            self.settings_model.set(key, variable.get())
-        self.settings_model.save()
+    def open_record(self, rownum=None):
+        if rownum is None:
+            record = None
+        else:
+            rownum = int(rownum)
+            try:
+                record = self.data_model.get_record(rownum)
+            except Exception as e:
+                messagebox.showerror(
+                    title='Error',
+                    message='Problem reading file',
+                    detail=str(e)
+                )
+                return
+        self.recordform.load_record(rownum, record)
+        self.recordform.tkraise()
 
     def on_save(self):
+        """Handles save button clicks"""
+
         # Check for errors first
         errors = self.recordform.get_errors()
         if errors:
-            error_string = f'Cannot save, error in fields: {", ".join(errors.keys())}'
-            error_string = error_string[:90] + '...' if len(error_string) > 90 else error_string
-            self.status.set(error_string)
-            message = 'Cannot save record'
-            error_list = '\n  * '.join(errors.keys())
-            detail = f'The following fields have errors: \n  * {error_list}'
+            message = "Cannot save record"
+            detail = "The following fields have errors: \n  * {}".format(
+                '\n  * '.join(errors.keys())
+            )
+            self.status.set(
+                "Cannot save, error in fields: {}"
+                .format(', '.join(errors.keys()))
+            )
             messagebox.showerror(title='Error', message=message, detail=detail)
+
             return False
 
-        filename = self.filename.get()
-        model = m.CSVModel(filename)
         data = self.recordform.get()
-        model.save_record(data)
-        self.records_saved += 1
-        self.status.set(f'{self.records_saved} records saved this session')
-        self.recordform.reset()
+        rownum = self.recordform.current_record
+        try:
+            self.data_model.save_record(data, rownum)
+        except IndexError as e:
+            messagebox.showerror(
+                title='Error',
+                message='Invalid row specified',
+                detail=str(e)
+            )
+            self.status.set('Tried to update invalid row')
+
+        except Exception as e:
+            messagebox.showerror(
+                title='Error',
+                message='Problem saving record',
+                detail=str(e)
+            )
+            self.status.set('Problem saving record')
+
+        else:
+            self.records_saved += 1
+            self.status.set(
+                "{} records saved this session".format(self.records_saved)
+            )
+            self.populate_recordlist()
+            # Only reset the form when we're appending records
+            if self.recordform.current_record is None:
+                self.recordform.reset()
+
 
     def on_file_select(self):
         """Handle the file->select action from the menu"""
@@ -96,9 +142,36 @@ class Application(tk.Tk):
         filename = filedialog.asksaveasfilename(
             title='Select the target file for saving records',
             defaultextension='.csv',
-            filetypes=[('Comma-Separated Values', '*.csv *.CSV')]
+            filetypes=[('CSV', '*.csv *.CSV')]
         )
-
         if filename:
             self.filename.set(filename)
             self.data_model = m.CSVModel(filename=self.filename.get())
+            self.populate_recordlist()
+
+    def save_settings(self, *args):
+        """Save the current settings to a preferences file"""
+
+        for key, variable in self.settings.items():
+            self.settings_model.set(key, variable.get())
+        self.settings_model.save()
+
+    def load_settings(self):
+        """Load settings into our self.settings dict."""
+
+        vartypes = {
+            'bool': tk.BooleanVar,
+            'str': tk.StringVar,
+            'int': tk.IntVar,
+            'float': tk.DoubleVar
+        }
+
+        # create our dict of settings variables from the model's settings.
+        self.settings = {}
+        for key, data in self.settings_model.variables.items():
+            vartype = vartypes.get(data['type'], tk.StringVar)
+            self.settings[key] = vartype(value=data['value'])
+
+        # put a trace on the variables so they get stored when changed.
+        for var in self.settings.values():
+            var.trace('w', self.save_settings)
